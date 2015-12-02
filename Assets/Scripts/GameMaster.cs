b0c65729fd1d25b3611c3dbcdf49700f3b1d3226
hiekichi <hiekichi@gmail.com>
2015-12-02 22:49:37 +0900

    Create Map.txt (1)

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -37,13 +37,13 @@ public class GameMaster : MonoBehaviour {
 		Vector3 v = new Vector3 ();
 		for (int x = -320; x <= 320; x += 32) {
 			v.x = x;
-			v.y = -192;
-			Instantiate (BlockTr, v, transform.rotation);  
+			//v.y = -192;
+			//Instantiate (BlockTr, v, transform.rotation);  
 			v.y = 192;
 			Instantiate (BlockTr, v, transform.rotation);  
 		}
 		// 周りの壁(2) 左右の端
-		for (int y = -160; y <= 160; y += 32) {
+		for (int y = -192; y <= 160; y += 32) {
 			v.x = -320;
 			v.y = y;
 			Instantiate (BlockTr, v, transform.rotation);  
